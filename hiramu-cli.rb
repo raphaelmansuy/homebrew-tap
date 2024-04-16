5e965944171aa6e859b988a72eceff59178b66e2
Raphael MANSUY <raphael.mansuy.partner@decathlon.com>
2024-04-16 14:05:27 +0800

    update

--- hiramu-cli.rb
+++ hiramu-cli.rb
@@ -1,7 +1,7 @@
 class HiramuCli < Formula
   desc "A CLI tool for interacting with LLMs"
   homepage "https://github.com/raphaelmansuy/hiramu-cli"
-  version "0.1.7"
+  version "v0.1.7"
   url "https://github.com/raphaelmansuy/hiramu-cli/archive/refs/tags/#{version}.tar.gz"
   sha256 "REPLACE_WITH_ACTUAL_SHA256_HASH"
   license "Apache-2.0"
