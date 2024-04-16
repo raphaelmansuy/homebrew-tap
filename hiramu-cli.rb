b8feb1cdf2df15889332bbf9a2fff63eb1df6022
Raphael MANSUY <raphael.mansuy.partner@decathlon.com>
2024-04-16 13:44:43 +0800

    add github action workflow

--- hiramu-cli.rb
+++ hiramu-cli.rb
@@ -1,9 +1,9 @@
 class HiramuCli < Formula
   desc "A CLI tool for interacting with LLMs"
-  homepage "https://github.com/yourusername/hiramu-cli"
-  url "https://github.com/raphaelmansuy/hiramu-cli/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "c311ec581e79d3773088126ca849e9c6283c52b5f0d3d4e6e00a9e66681f3301"
-  license "Apache 2"
+  homepage "https://github.com/raphaelmansuy/hiramu-cli"
+  url "https://github.com/raphaelmansuy/hiramu-cli/archive/refs/tags/#{version}.tar.gz"
+  sha256 :no_check
+  license "Apache-2.0"
 
   depends_on "rust" => :build
 
