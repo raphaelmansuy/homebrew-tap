363c04f7df74e17efba48f9bc33aef7553254903
Raphael MANSUY <raphael.mansuy.partner@decathlon.com>
2024-04-16 14:56:38 +0800

    Update hiramu-cli.rb

--- hiramu-cli.rb
+++ hiramu-cli.rb
@@ -2,7 +2,7 @@ class HiramuCli < Formula
   desc "A CLI tool for interacting with large language models"
   homepage "https://github.com/raphaelmansuy/hiramu-cli"
   url "https://github.com/raphaelmansuy/hiramu-cli/archive/refs/tags/v0.1.8.tar.gz"
-  sha256 sha256sum("https://github.com/raphaelmansuy/hiramu-cli/archive/refs/tags/v0.1.8.tar.gz")
+  sha256 "8042ed4a64d9e26a27d3626e6688da8f6b34d4b95803aed41a82573a817bcf22"
   license "Apache-2.0"
 
   depends_on "rust" => :build
