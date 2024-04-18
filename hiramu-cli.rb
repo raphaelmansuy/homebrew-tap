feebebb889c8b4271020790f220f6f5054b1c55a
Raphael MANSUY <raphael.mansuy.partner@decathlon.com>
2024-04-18 14:54:31 +0800

    Update hiramu-cli to version 0.1.14

--- hiramu-cli.rb
+++ hiramu-cli.rb
@@ -1,7 +1,7 @@
 class HiramuCli < Formula
   desc "A CLI tool for interacting with large language models"
   homepage "https://github.com/raphaelmansuy/hiramu-cli"
-  url "https://github.com/raphaelmansuy/hiramu-cli/archive/refs/tags/v0.1.10.tar.gz"
+  url "https://github.com/raphaelmansuy/hiramu-cli/archive/refs/tags/v0.1.14.tar.gz"
   sha256 "59d72f77319f592833bc97a51ec4dda86003662e244e8b928402616b4bd6d917"
   license "Apache-2.0"
 
