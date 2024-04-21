a8c2447fc8861d2245617be9ba9640eac26dcba9
Raphael MANSUY <raphael.mansuy.partner@decathlon.com>
2024-04-21 15:15:31 +0800

    Bump version to v0.1.19 and update SHA256 checksum

--- hiramu-cli.rb
+++ hiramu-cli.rb
@@ -1,8 +1,8 @@
 class HiramuCli < Formula
   desc "A CLI tool for interacting with large language models"
   homepage "https://github.com/raphaelmansuy/hiramu-cli"
-  url "https://github.com/raphaelmansuy/hiramu-cli/archive/refs/tags/v0.1.15.tar.gz"
-  sha256 "720f9292b76cc34ed4ddc2b71eaf0b600f3f7d4a3600bae8b2c355834f9356d3"
+  url "https://github.com/raphaelmansuy/hiramu-cli/archive/refs/tags/v0.1.19.tar.gz"
+  sha256 "4f9affef11d4dc9dca4044b4b76ecddfe613b585100b7f73a46161b9f308564d"
   license "Apache-2.0"
 
   depends_on "rust" => :build
