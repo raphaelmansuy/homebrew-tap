fa455640644484701a2aa86d077f99420dba40d0
Raphael MANSUY <raphael.mansuy@gmail.com>
2026-04-13 12:17:55 +0800

    edgecrab 0.4.0

--- Formula/edgecrab.rb
+++ Formula/edgecrab.rb
@@ -1,16 +1,16 @@
 class Edgecrab < Formula
   desc "AI-native terminal agent with 74+ tools, code execution, and 15 messaging gateways"
   homepage "https://github.com/raphaelmansuy/edgecrab"
-  version "0.3.3"
+  version "0.4.0"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/raphaelmansuy/edgecrab/releases/download/v0.3.3/edgecrab-aarch64-apple-darwin.tar.gz"
-      sha256 "ae9347a4a37d52581c35905fae7881cf58e711b05b7f5f33c80b7b5d538c94e6"
+      url "https://github.com/raphaelmansuy/edgecrab/releases/download/v0.4.0/edgecrab-aarch64-apple-darwin.tar.gz"
+      sha256 "64eb190368dc65f6f93e419fe1b8fdbd512f36717841d83e79e17fd5e6665cf2"
     else
-      url "https://github.com/raphaelmansuy/edgecrab/releases/download/v0.3.3/edgecrab-x86_64-apple-darwin.tar.gz"
-      sha256 "e5daf8878c39ead764aece6b3699744ad7c8b3c76c261a44125c547e26a5ea96"
+      url "https://github.com/raphaelmansuy/edgecrab/releases/download/v0.4.0/edgecrab-x86_64-apple-darwin.tar.gz"
+      sha256 "6fbe6bdd7b2e27f2aeb7ba83d5c884e7d92b2c431a2e7f6fddd502de97d3fa7b"
     end
   end
 
